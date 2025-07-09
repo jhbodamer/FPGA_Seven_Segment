1f1ef10a2f6bcc22a8e184b1d213c00985d8d83d
jhbodamer <johnhbodamer@gmail.com>
2025-07-09 15:14:59 -0400

    fsm written but switching too fast
    going to reduce switching speed so hopefully the digits appear

--- sevenseg/sevenseg.srcs/sources_1/new/sevenseg.vhd
+++ sevenseg/sevenseg.srcs/sources_1/new/sevenseg.vhd
@@ -33,7 +33,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity sevenseg is
     Port ( 
-           --clk : in STD_LOGIC;
+            clk : in STD_LOGIC;
            --btn0 : in STD_LOGIC;
            --btn1 : in STD_LOGIC;
            digit : out STD_LOGIC_VECTOR (3 downto 0);
@@ -41,9 +41,38 @@ entity sevenseg is
 end sevenseg;
 
 architecture Behavioral of sevenseg is
+   
+   --signal which tracks which digit and FSM state the cycle is on 
+    type digit_labels is(
+                first, second, third, fourth
+                );
+    signal current_digit : digit_labels;
 
 begin
-            digit <= "1110";
-            led <= X"01";
+            -- Finite State Machine Process
+            Main_Process: process (clk)
+                begin   
+                    if (rising_edge(clk)) then
+                        case (current_digit) is
+                            when first => 
+                                led <= "01010100"; --letter  n
+                                digit <= "1110"; -- right most digit
+                                current_digit <= second;
+                            when second => 
+                                led <= "01110100"; -- letter h
+                                digit <= "1101";
+                                current_digit <= third;
+                            when third => 
+                                led <= "01011100"; -- letter o
+                                digit <= "1011";
+                                current_digit <= fourth;
+                            when fourth => 
+                                led <= "00011111"; -- letter J
+                                digit <= "0111";
+                                current_digit <= first;
+                            when others => current_digit <= first;
+                        end case;
+                    end if;
+               end process;
 
 end Behavioral;
